44f2220d775e70c829f4a109c773b45d9a67791a
nstoddard <nstoddard@proton.me>
2024-11-11 22:16:59 -0500

    test script works

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -6,6 +6,17 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node,node2)
+    
+    def test_neq(self):
+        node3 = TextNode("This is a text node", TextType.ITALIC)
+        node4 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node3,node4)
+
+    def test_eq1(self):
+        node = TextNode("This is a text node", TextType.BOLD,"http://url.com")
+        node2 = TextNode("This is different text", TextType.CODE)
+        self.assertNotEqual(node,node2)
+    
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -8,17 +8,18 @@ class TextType(Enum):
     LINK = "Link text"
     IMAGE = "Image text"
 
-class TextNode(text, text_type,url=None):
-    self.text = text
-    self.text_type = text_type
-    self.url = url
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
 
-    def __eg__(self,other_text):
+    def __eq__(self,other_text):
         if (self.text_type == other_text.text_type) and (self.text == other_text.text) and (self.url == other_text.url):
             return True
         return False
     
     def __repr__(self):
-        new_string = "TextNode(" + str(text) +", "+str(self.text_type)+", "+str(url)")"
+        new_string = "TextNode(" + str(self.text) +", "+str(self.text_type)+", "+str(self.url) + ")"
         return new_string
     

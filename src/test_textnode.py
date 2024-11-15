2144c273518fc0b3af708bb690c47268c15601e8
nstoddard <nstoddard@proton.me>
2024-11-14 23:04:34 -0500

    added split_nodes_delimiter function to textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -17,6 +17,49 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is different text", TextType.CODE)
         self.assertNotEqual(node,node2)
     
+class TestSplitNodesDelimiter(unittest.TestCase):
+    
+    def test_single_delimiter(self):
+        nodes = [TextNode("Hello, **bold** world!", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes,"**",TextType.BOLD)
+        expected = [
+            TextNode("Hello, ", TextType.NORMAL),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" world!", TextType.NORMAL)
+        ]
+        self.assertEqual(result, expected)
+    
+    def test_multiple_delimiters(self):
+        nodes = [TextNode("**Bold** and _italic_ text", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        result = split_nodes_delimiter(result, "_", TextType.ITALIC)
+        expected = [
+            TextNode("Bold",TextType.BOLD),
+            TextNode(" and ",TextType.NORMAL),
+            TextNode("italic",TextType.ITALIC),
+            TextNode(" text",TextType.NORMAL)
+        ]
+        self.assertEqual(result,expected)
+
+    def test_no_delimiter(self):
+        nodes = [TextNode("Plain text with no formatting", TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes,"**",TextType.BOLD)
+        expected = [TextNode("Plain text with no formatting", TextType.NORMAL)]
+        self.assertEqual(result,expected)
+
+    def test_text_ends_with_delimiter(self):
+        nodes = [TextNode("Bold at the end**",TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes,"**",TextType.BOLD)
+        expected = [TextNode("Bold at the end",TextType.NORMAL)]
+        self.assertEqual(result,expected)
+
+    def test_empty_text(self):
+        nodes = [TextNode("",TextType.NORMAL)]
+        result = split_nodes_delimiter(nodes,"**",TextType.BOLD)
+        expected = [TextNode("",TextType.NORMAL)]
+        self.assertEqual(result,expected)
+
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -23,3 +23,28 @@ class TextNode():
         new_string = "TextNode(" + str(self.text) +", "+str(self.text_type)+", "+str(self.url) + ")"
         return new_string
     
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        remaining_text = old_node.text
+        if len(remaining_text) > 0:
+            current_type = old_node.text_type
+            while delimiter in remaining_text:
+                pre_text, _, post_text = remaining_text.partition(delimiter)
+                if pre_text:
+                    new_nodes.append(TextNode(pre_text,current_type))
+                current_type = text_type
+                remaining_text = post_text
+                if not remaining_text:
+                    current_type = old_node.text_type
+                    break
+            current_type = old_node.text_type
+            if remaining_text:
+                new_nodes.append(TextNode(remaining_text, current_type))
+        else:
+            new_nodes.append(TextNode("",old_node.text_type)) 
+    
+    return new_nodes
+
+
+
